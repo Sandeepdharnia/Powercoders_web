5fd4503b21f9893f1dba63d01766cf99da791228
sandeepdharnia <sandeep.dharnia@powercoders.org>
2025-04-11 00:04:29 +0200

    react added

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -5,6 +5,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headers = screen.getAllByText(/delicious recipes/i);
+  expect(headers.length).toBeGreaterThan(0);
 });
