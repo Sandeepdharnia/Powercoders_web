973a6e2bbd2990010bd82d3fb9f074fd9f324a28
Sandeep Dharnia <45833647+Sandeepdharnia@users.noreply.github.com>
2025-04-19 16:46:26 +0200

    Update settings.py

--- recipesapi/recipesapi/settings.py
+++ recipesapi/recipesapi/settings.py
@@ -184,10 +184,13 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5500',  # Frontend origin
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:5500',  # Frontend origin
+# ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://cookinglovepassion.netlify.app",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5500',
