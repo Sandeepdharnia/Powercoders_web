fe93f5db01fec27a085fd8cf2a901aa522b51973
Sandeep Dharnia <45833647+Sandeepdharnia@users.noreply.github.com>
2025-05-01 16:47:54 +0200

    Update urls.py

--- recipesapi/recipesapi/urls.py
+++ recipesapi/recipesapi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('api2.urls')),
+    path('', lambda request: HttpResponse("Welcome to the API!")),
 ]
