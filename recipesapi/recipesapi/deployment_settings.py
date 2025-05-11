47a47a4d4246d60e1bdd4f25098d709cd08b5e66
sandeepdharnia <sandeep.dharnia@powercoders.org>
2025-05-11 22:42:23 +0200

    deployment_setting updated

--- recipesapi/recipesapi/deployment_settings.py
+++ recipesapi/recipesapi/deployment_settings.py
@@ -6,7 +6,8 @@ from .settings import*
 from .settings import BASE_DIR
 
 
-ALLOWED_HOSTS = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
+
 
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
 
